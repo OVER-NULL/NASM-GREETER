72db5bbfe81349455422757304d0236a27d780b1
OVER-NULL <53993161+OVER-NULL@users.noreply.github.com>
2019-08-19 00:50:37 -0700

    Add files via upload

--- driver.asm
+++ driver.asm
@@ -1,7 +1,14 @@
-; nasm -f elf64 hw.asm && ld hw.o -o hw && ./hw 
-; Or use the above to compile link and run 
+; nasm -f elf64 driver.asm && ld driver.o -o driver
+; use the above to compile and link
+
+; ./driver
+; use the above to run the program 
+
+; file descriptor passes to kernal so kernal accesses file through call system 
 
 SECTION .data
+
+    ; [name] then db for define bytes 
     
     msg     db      "What is your name: "
     msgn    db      10, "Hello, "            ; 10 for newline or 0Ah 
@@ -10,63 +17,65 @@ SECTION .bss
     
     name: RESB 64 ; reserve 64 bytes for name variable 
     
+    
+    ; make a conditional incase 64 bytes is overloaded 
+    
 SECTION .text
     
     global _start
  
 _start:
     
-    call _msg         ;
-    call _getName     ;
-    call _msgn        ;
-    call _printName   ;
+    call _msg         ; call function 
+    call _getName     ; call function 
+    call _msgn        ; call function 
+    call _printName   ; call function 
  
     mov     rax, 60   ; exit 
-    mov     rdi, 0    ; exit with success
+    mov     rdi, 0    ; error code 0 for exit with success
     
-    syscall ; _
+    syscall           ; _
     
 _getName:
 
-    mov     rax, 0    ; 
-    mov     rdi, 0    ; 
-    mov     rsi, name ; 
-    mov     rdx, 16   ; 
+    mov     rax, 0    ; syscall read input by moving 0 into rax 
+    mov     rdi, 0    ; file descriptor 0 is stdin 
+    mov     rsi, name ; load memory address into rsi of name 
+    mov     rdx, 16   ; tell register how many bytes to allocate 
     
-    syscall           ;
+    syscall           ; _
     
-    ret               ;
+    ret               ; return to where function was called 
     
 _msg: 
 
-    mov     rax, 1    ;
-    mov     rdi, 1    ;
-    mov     rsi, msg  ;
-    mov     rdx, 19   ;
+    mov     rax, 1    ; syscall write output by moving 1 into rax 
+    mov     rdi, 1    ; file descriptor 1 is stdout
+    mov     rsi, msg  ; load memory address into rsi of msg 
+    mov     rdx, 19   ; tell register how many bytes to allocate 
     
     syscall           ; _ 
     
-    ret               ; 
+    ret               ; return to where function was called 
     
 _msgn: 
 
-    mov     rax, 1    ;
-    mov     rdi, 1    ;
-    mov     rsi, msgn ;
-    mov     rdx, 8    ; 
+    mov     rax, 1    ; syscall write output 1 into rax
+    mov     rdi, 1    ; file descriptor 1 is stdout   
+    mov     rsi, msgn ; load memory address into rsi of msgn 
+    mov     rdx, 8    ; tell register how many bytes to allocate 
     
-    syscall 
+    syscall           ; _
     
-    ret 
-
+    ret               ; return to where function was called 
 
 _printName: 
 
-    mov     rax, 1    ;
-    mov     rdi, 1    ;
-    mov     rsi, name ;
-    mov     rdx, 64   ; 
+    mov     rax, 1    ; syscall write output 1 into rax 
+    mov     rdi, 1    ; file descriptor 1 is stdout
+    mov     rsi, name ; load memory address into rsi of name  
+    mov     rdx, 64   ; tell register how many bytes to allocate 
     
-    syscall           ;
+    syscall           ; _
     
-    ret               ;    
+    ret               ; return to where function was called 
