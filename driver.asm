adb29d5d90aafaa2c6c1cd2fd9fb5c8e86dd0662
OVER-NULL <53993161+OVER-NULL@users.noreply.github.com>
2019-08-19 20:29:16 -0700

    Add files via upload

--- driver.asm
+++ driver.asm
@@ -8,15 +8,17 @@
 
 SECTION .data
 
-    ; [name] then db for define bytes 
+    ; [name] then db for define bytes then message and newline
     
     msg     db      "What is your name: "
-    msgn    db      10, "Hello, "            ; 10 for newline or 0Ah 
+    msgn    db      10, "Hello, " ; 10 for newline or 0Ah 
+    wel     db      10, "Press enter a for nice message: " ; 10 for newline 0Ah
+    fun     db      10, "Have a nice day, " ; 10 for newline 0Ah
 
 SECTION .bss
     
     name: RESB 64 ; reserve 64 bytes for name variable 
-    
+    ent: RESB 1 ; reserve 1 byte for newline 
     
     ; make a conditional incase 64 bytes is overloaded 
     
@@ -30,18 +32,22 @@ _start:
     call _getName     ; call function 
     call _msgn        ; call function 
     call _printName   ; call function 
+    call _wel         ; call function
+    call _entered     ; call function 
+    call _printFun    ; call function 
+    call _printName   ; call function 
  
     mov     rax, 60   ; exit 
     mov     rdi, 0    ; error code 0 for exit with success
     
     syscall           ; _
-    
+
 _getName:
 
     mov     rax, 0    ; syscall read input by moving 0 into rax 
     mov     rdi, 0    ; file descriptor 0 is stdin 
     mov     rsi, name ; load memory address into rsi of name 
-    mov     rdx, 16   ; tell register how many bytes to allocate 
+    mov     rdx, 64   ; tell register how many bytes to allocate 
     
     syscall           ; _
     
@@ -79,3 +85,36 @@ _printName:
     syscall           ; _
     
     ret               ; return to where function was called 
+    
+_wel:
+
+    mov     rax, 1   ; syscall write output 1 into rax
+    mov     rdi, 1   ; file descriptor 1 is stdout 
+    mov     rsi, wel ; load memory address into rsi of wel
+    mov     rdx, 33  ; tell register how many bytes to allocate
+    
+    syscall          ; _ 
+    
+    ret              ; return to where function was called
+
+_entered: 
+
+    mov     rax, 0   ; syscall read input by moving 0 into rax 
+    mov     rdi, 0   ; file descriptor 0 is stdin 
+    mov     rsi, ent ; load memory address into rsi of name 
+    mov     rdx, 1   ; tell register how many bytes to allocate 
+    
+    syscall          ; _
+    
+    ret              ; return to where function was called
+        
+_printFun:
+
+    mov     rax, 1   ; syscall write output 1 into rax
+    mov     rdi, 1   ; file descriptor 1 is stdout 
+    mov     rsi, fun ; load memory address into rsi of fun
+    mov     rdx, 18  ; tell register how many bytes to allocate
+    
+    syscall          ; _ 
+    
+;     ret              ; return to where function was called
